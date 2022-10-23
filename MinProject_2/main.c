3c4b7ea5a98f22f88ceb01ba72ef0f724087fa3f
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2022-10-23 11:26:56 -0500

    Update main.c

--- MinProject_2/main.c
+++ MinProject_2/main.c
@@ -27,28 +27,34 @@ int cnt = 0; // count init to 0
 int TOTAL = 0;
 int array[10000];
 // void function for semaphore
-void *pThreadFunction(void *p) {
+void *pThreadFunction(void *p) 
+{
 
   long tid = (long)p;
   pthread_mutex_lock(&mutex);
   // thread waits with semaphore for other to turn up
-  if (tid != cnt) {
+  if (tid != cnt) 
+  {
     pthread_cond_wait(&cond[tid], &mutex);
   }
   printf("TID is : %ld\n", tid);
   // this is one of the two either the one running now or the commented out
   
 
-  for (int i = (tid * 2000); i < ((tid * 2000)+2000); i++) {
+  for (int i = (tid * 2000); i < ((tid * 2000)+2000); i++) 
+  {
     // for (int i = tid * 2000; i > (tid * 2000 - 1); i--)
     // {
     TOTAL = TOTAL + array[i];
   }
 
   printf("Semaphore is  : %d\n", TOTAL);
-  if (cnt < NUM_OF_THREADS - 1) {
+  if (cnt < NUM_OF_THREADS - 1) 
+  {
     cnt++;
-  } else {
+  }
+  else 
+  {
     cnt = 0;
   }
   // free up for the next waiting thread
@@ -57,19 +63,23 @@ void *pThreadFunction(void *p) {
   pthread_mutex_unlock(&mutex);
 }
 // main driver for program
-int main() {
+int main() 
+{
   pthread_t threads[NUM_OF_THREADS];
   // initialize the array of 10000 values
-  for (int i = 0; i < 10000; i++) {
+  for (int i = 0; i < 10000; i++) 
+  {
     array[i] = i % 257; // initialize i%257 inside of the array of values
   }
   // now we need to create the threads
-  for (int i = 0; i < NUM_OF_THREADS; i++) {
+  for (int i = 0; i < NUM_OF_THREADS; i++) 
+  {
     // using pthread create to creat the thread
     pthread_create(&threads[i], NULL, pThreadFunction, (void *)i);
   }
   // joion the threads
-  for (int i = 0; i < NUM_OF_THREADS; i++) {
+  for (int i = 0; i < NUM_OF_THREADS; i++) 
+  {
     pthread_join(threads[i], NULL);
   }
   // now we get the TOTAL
