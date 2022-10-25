37f9d6c4c90826168aaf2c1f4baac3dc1414900f
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2022-10-24 19:56:07 -0500

    Update main.c

--- MinProject_2/main.c
+++ MinProject_2/main.c
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////
 //                                                //
 // Authors    : Ethan Coyle, Jonathan Hogan and   //
-//              Dymon Browne                       //
+//              Dymon Browne                      //
 //                                                //
 // Teacher    : Dr. Passos                        //
 // Class      : Intro to Operating Systems        //
@@ -11,85 +11,132 @@
 // using thread counts  for unix threading        //
 ////////////////////////////////////////////////////
 
-// include methods
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-// number of threads for the program be 5
 #define NUM_OF_THREADS 5
+#define SIZE 10000
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mx;
 pthread_cond_t cond[5];
-int cnt = 0; // count init to 0
-// dglobal variables
-int TOTAL = 0;
-int array[10000];
-// void function for semaphore
-void *pThreadFunction(void *p) 
-{
+int myMutex = 1;
 
-  long tid = (long)p;
-  pthread_mutex_lock(&mutex);
-  // thread waits with semaphore for other to turn up
-  if (tid != cnt) 
-  {
-    pthread_cond_wait(&cond[tid], &mutex);
-  }
-  printf("TID is : %ld\n", tid);
-  // this is one of the two either the one running now or the commented out
+int total = 0;
+int semaphoreTotal = 0;
+int mutexTotal = 0;
+
+int theArray[SIZE];
+
+void wait(S){
+    while (S < 1){};
+    S--;
+}
+
+void post(S){
+    S++;
+}
+
+void sem_sum(void *thread_id){
+    long sum = 0;
+    long tid = (long)thread_id;
+    long start = ((tid - 1) * 2000);
+    long end = start + 2000;
+
+   
+
+    for (int i = start; i < end; i++){
+        sum += theArray[i];
+    }
+
+    wait(myMutex);
   
+    semaphoreTotal += sum;
+    printf("Thread %ld: Sum is %ld using a semaphore\n", tid, sum);
 
-  for (int i = (tid * 2000); i < ((tid * 2000)+2000); i++) 
-  {
-    // for (int i = tid * 2000; i > (tid * 2000 - 1); i--)
-    // {
-    TOTAL = TOTAL + array[i];
-  }
-
-  printf("Semaphore is  : %d\n", TOTAL);
-  if (cnt < NUM_OF_THREADS - 1) 
-  {
-    cnt++;
-  }
-  else 
-  {
-    cnt = 0;
-  }
-  // free up for the next waiting thread
-  pthread_cond_signal(&cond[cnt]);
-  // the executing thread unlock the mutex
-  pthread_mutex_unlock(&mutex);
+    post(myMutex);
 }
-// main driver for program
-int main() 
+
+void mutex_sum(void *thread_id){
+    long sum = 0;
+    long tid = (long)thread_id;
+    long start = ((tid - 1) * 2000);
+    long end = start + 2000;
+
+   
+
+    for (int i = start; i < end; i++){
+        sum += theArray[i];
+    }
+
+    pthread_mutex_lock(&mx);
+  
+    mutexTotal =mutexTotal+ sum;
+    printf("Thread %ld: Sum is %ld using a mutex\n", tid, sum);
+
+    pthread_mutex_unlock(&mx);
+}
+
+void no_sem_sum(void *thread_id){
+    long sum = 0;
+    long tid = (long)thread_id;
+    long start = ((tid - 1) * 2000);
+    long end = start + 2000;
+
+    for (int i = start; i < end; i++){
+        sum += theArray[i];
+    }
+    total += sum;
+
+    printf("Thread %ld: Sum is %ld without using a semaphore\n", tid, sum);
+
+}
+
+int main()
 {
-  pthread_t threads[NUM_OF_THREADS];
-  // initialize the array of 10000 values
-  for (int i = 0; i < 10000; i++) 
-  {
-    array[i] = i % 257; // initialize i%257 inside of the array of values
-  }
-  // now we need to create the threads
-  for (int i = 0; i < NUM_OF_THREADS; i++) 
-  {
-    // using pthread create to creat the thread
-    pthread_create(&threads[i], NULL, pThreadFunction, (void *)i);
-  }
-  // joion the threads
-  for (int i = 0; i < NUM_OF_THREADS; i++) 
-  {
-    pthread_join(threads[i], NULL);
-  }
-  // now we get the TOTAL
-  printf("TOTAL is  : %d\n", TOTAL);
-  // now we close the threads
-  pthread_exit(NULL);
-  return 0;
-}
+    pthread_t threads[NUM_OF_THREADS];
+    int rc;
+    long t;
+    void *status;
+    pthread_attr_t attr;
+    printf("\nThe Sums for each are shown below showing w/out\n");
+    printf("semaphores, with semaphores and mutex semaphore\n");
+    printf("-------------------------------------------------\n\n");
+    //pthread_mutex_init(&mx,NULL);
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    
+    for(int i = 1; i < SIZE+1; i++){
+        theArray[i-1] = i%257;
+    }
 
-// only thing left is make sure values of array are initialized i%257 i think
-// that I got that done and then the only other thing is where passos specified
-// that the value range cshould be 2000-3999 those are the only two things this
-// is lacking and the program will be done
+    for(int tid = 1; tid <= NUM_OF_THREADS; tid++){
+        //Function without a semaphore
+        rc = pthread_create(&threads[tid - 1], &attr, no_sem_sum, (void *)tid);
+        //Following function uses a semaphore
+        rc = pthread_create(&threads[tid - 1], &attr, sem_sum, (void *)tid);
+        //Following function uses a mutex
+        rc = pthread_create(&threads[tid - 1], &attr, mutex_sum, (void *)tid);
+        if (rc){
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
+
+    pthread_attr_destroy(&attr);
+
+    for(int i=0; i<NUM_OF_THREADS; i++){
+        pthread_join(threads[i], &status);
+    }
+    printf("\nThe Totals for all elements of the arrays are shown below\n");
+    printf("using mutex, with semaphores, and without the semaphores   \n");
+    printf("-----------------------------------------------------------\n\n");
+    printf("The total of all elements in the array is: %d\n", total);
+    printf("The total of all elements in the array using a semaphore is: %d\n", semaphoreTotal);
+    printf("The total of all elements in the array using a mutex is: %d\n", mutexTotal);
+   
+    pthread_exit(NULL);
+    return 0;
+
+} 
